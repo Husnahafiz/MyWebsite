75a8139e3722908c73b8996bc7b330aeee107e88
Nurul Husna <husna.27998@gmail.com>
2022-02-02 10:16:46 +0800

    Sorting

--- MyWebsite/Pages/User/Index.cshtml.cs
+++ MyWebsite/Pages/User/Index.cshtml.cs
@@ -12,18 +12,48 @@ namespace MyWebsite.Pages.User
 {
     public class IndexModel : PageModel
     {
-        private readonly MyWebsite.Data.MyWebsiteContext _context;
+        private readonly MyWebsiteContext _context;
+        private string sortOrder;
 
-        public IndexModel(MyWebsite.Data.MyWebsiteContext context)
+        public IndexModel(MyWebsiteContext context)
         {
             _context = context;
         }
 
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
         public IList<ListUser> ListUser { get;set; }
 
         public async Task OnGetAsync()
         {
-            ListUser = await _context.ListUser.ToListAsync();
+            //ListUser = await _context.ListUser.ToListAsync();
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<ListUser> usersIQ = from s in _context.ListUser
+                                             select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    usersIQ = usersIQ.OrderByDescending(s => s.FullName);
+                    break;
+                case "Date":
+                    usersIQ = usersIQ.OrderBy(s => s.DateJoined);
+                    break;
+                case "date_desc":
+                    usersIQ = usersIQ.OrderByDescending(s => s.DateJoined);
+                    break;
+                default:
+                    usersIQ = usersIQ.OrderBy(s => s.FullName);
+                    break;
+            }
+
+            ListUser = await usersIQ.AsNoTracking().ToListAsync();
         }
     }
 }
+
