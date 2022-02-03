eea6beda0d1496ca97a1d8cd0c2149b9b6c69db6
Nurul Husna <husna.27998@gmail.com>
2022-02-04 01:08:43 +0800

    Tukar tempat

--- MyWebsite/Pages/Index.cshtml.cs
+++ MyWebsite/Pages/Index.cshtml.cs
@@ -1,25 +1,75 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyWebsite.Data;
+using MyWebsite.Models;
 
 namespace MyWebsite.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public void OnGet()
+        //{
+
+        //}
+        private readonly MyWebsiteContext _context;
+        //private string sortOrder;
+
+        public IndexModel(MyWebsiteContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public IList<ListUser> ListUser { get; set; }
 
+        public async Task OnGetAsync(string sortOrder, string searchString)
+        {
+            //ListUser = await _context.ListUser.ToListAsync();
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            CurrentFilter = searchString;
+
+            IQueryable<ListUser> usersIQ = from s in _context.ListUsers
+                                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                usersIQ = usersIQ.Where(s => s.FullName.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    usersIQ = usersIQ.OrderByDescending(s => s.FullName);
+                    break;
+                case "Date":
+                    usersIQ = usersIQ.OrderBy(s => s.DateJoined);
+                    break;
+                case "date_desc":
+                    usersIQ = usersIQ.OrderByDescending(s => s.DateJoined);
+                    break;
+                default:
+                    usersIQ = usersIQ.OrderBy(s => s.FullName);
+                    break;
+            }
+
+            ListUser = await usersIQ.AsNoTracking().ToListAsync();
         }
     }
 }
+
