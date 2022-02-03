15db1fe4a2dc66b46b1b4196390a16a6668b07d6
Nurul Husna <husna.27998@gmail.com>
2022-02-03 23:49:57 +0800

    Required

--- MyWebsite/Models/ListUser.cs
+++ MyWebsite/Models/ListUser.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyWebsite.Models
 {
     public class ListUser
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string FullName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Position")]
         public string Position { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date Joined")]
         public DateTime DateJoined { get; set; }
 
-        //public ICollection<Enrollment> Enrollments { get; set; }
+        
     }
 }
